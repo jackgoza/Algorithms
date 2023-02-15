2880e34749432df3fba9f016a2b060d7add8db32
Jack Goza <john.goza@ceva.com>
2023-02-14 20:48:18 -0600

    Init project 1

---
+++ Lab1/Lab1.py
@@ -0,0 +1,107 @@
+# John Goza
+# Lab 1 - Strassen's algorithm
+import re
+
+matrix_a = [[2, 1], [1, 5]]
+matrix_b = [[6, 7], [4, 3]]
+
+# format_matrix(order: int, matrix: flat array)
+# takes a flat array of matrix values in row major order and maps them to a nested array
+# where each index of the array contains one row of the matrix
+def format_matrix(order, matrix):
+    formatted_matrix = []
+    # God bless python for being a zero-indexed language but dang this is annoying
+    for i in range(1, order + 1):
+        upper_bound = order * i
+        lower_bound = upper_bound - order
+        formatted_matrix.append(matrix[lower_bound:upper_bound])
+    return formatted_matrix
+
+
+# validate_input_matrix_constraints(matrix in form of [[a, b],[c, d]])
+#   checks that a given matrix is both n x n and that n is an exact power of 2
+#   returns: None, throws: Exception if either constraint is not met
+def validate_matrix_constraints(input_matrix):
+    row_count = len(input_matrix)
+
+    # todo: cite: https://stackoverflow.com/questions/57025836/how-to-check-if-a-given-number-is-a-power-of-two
+    if (row_count & (row_count - 1) != 0) or row_count == 0:
+        raise Exception("Invalid input given! Matrix is not an exact power of two!")
+
+    for row in input_matrix:
+        if len(row) != row_count:
+            raise Exception("Invalid input given! Rows and columns differ in length.")
+
+
+# A file with required input is provided. All input you create should be formatted the same: the
+# first line should contain the order of the matrix, then the first matrix, in row major order, then
+# the second matrix. This is followed by a blank line, then the order of the next matrix pair and
+# so on.
+def parse_matrix_file(filename):
+    try:
+        # todo: cite? https://docs.python.org/3/tutorial/inputoutput.html
+        file = open(filename, 'r', encoding='utf-8')
+        matrix_order_str = file.readline().strip()
+        first_matrix_string = file.readline().strip()
+        second_matrix_string = file.readline().strip()
+
+        reg = re.compile(r"(\d*)x(\d*)")
+        order_string = reg.search(matrix_order_str).groups()
+        if not order_string \
+                or order_string[0] != order_string[1] \
+                or not order_string[0].isdigit():
+            raise Exception(f"Order string is invalid! Given: {matrix_order_str}")
+
+        order = int(order_string[0])
+
+        if (order & (order - 1) != 0):
+            raise Exception(f"Order is not an exact power of two! Given: {matrix_order_str}")
+
+        first_flat_matrix = list(map(int, first_matrix_string.split()))
+        second_flat_matrix = list(map(int, second_matrix_string.split()))
+
+        first_matrix = format_matrix(order, first_flat_matrix)
+        second_matrix = format_matrix(order, second_flat_matrix)
+
+        validate_matrix_constraints(first_matrix)
+        validate_matrix_constraints(second_matrix)
+
+        return order, first_matrix, second_matrix
+    except Exception as e:
+        print("File parsing failed")
+        raise e
+
+
+def partition_matrix(order, whole_matrix):
+    # whole_matrix = [[1, 1, 1, 1, 11, 11, 11, 11], [2, 2, 2, 2, 22, 22, 22, 22], [3, 3, 3, 3, 33, 33, 33, 33], [4, 4, 4, 4, 44, 44, 44, 44],
+    #              [5, 5, 5, 5, 55, 55, 55, 55], [6, 6, 6, 6, 66, 66, 66, 66], [7, 7, 7, 7, 77, 77, 77, 77], [8, 8, 8, 8, 88, 88, 88, 88]]
+
+    half_len = int(order / 2)
+    # if half_len == 1:
+    #     return whole_matrix
+
+    quadrants = [[], [], [], []]
+
+    for i in range(0, order):
+        if i < half_len:
+            quadrants[0].append(whole_matrix[i][0:half_len])
+            quadrants[1].append(whole_matrix[i][half_len:])
+        else:
+            quadrants[2].append(whole_matrix[i][0:half_len])
+            quadrants[3].append(whole_matrix[i][half_len:])
+
+    return quadrants
+
+
+if __name__ == "__main__":
+    order = 2
+    matrix1 = [[2,1], [1,5]]
+    matrix2 = [[6,7], [4,3]]
+    # whole_matrix = [[1,1,11,11],[2,2,22,22],[3,3,33,33],[4,4,44,44]]
+    # whole_matrix = [[1, 1, 1, 1, 11, 11, 11, 11], [2, 2, 2, 2, 22, 22, 22, 22], [3, 3, 3, 3, 33, 33, 33, 33], [4, 4, 4, 4, 44, 44, 44, 44],
+    #              [5, 5, 5, 5, 55, 55, 55, 55], [6, 6, 6, 6, 66, 66, 66, 66], [7, 7, 7, 7, 77, 77, 77, 77], [8, 8, 8, 8, 88, 88, 88, 88]]
+    # order, matrix1, matrix2 = parse_matrix_file('input.txt')
+    split_matrix1 = partition_matrix(order, matrix1)
+    split_matrix2 = partition_matrix(order, matrix2)
+    print(split_matrix1)
+    print(split_matrix2)
