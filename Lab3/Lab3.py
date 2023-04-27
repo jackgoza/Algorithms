ddb3e132b8f97aeb404ca3a0e5710293fa448cd5
Jack Goza <john.goza@ceva.com>
2023-04-26 23:06:48 -0500

    get the walk algorithm working

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -6,7 +6,7 @@ import argparse
 
 def init_tables(m, n):
     b = [['-1' for _ in range(0, n)] for _ in range(0, m)]
-    c = [[None for _ in range(0, n+1)] for _ in range(0, m+1)]
+    c = [[None for _ in range(0, n + 1)] for _ in range(0, m + 1)]
     return b, c
 
 
@@ -15,18 +15,32 @@ def generate_lcs_table(x, y):
     n = len(y)
     b, c = init_tables(m, n)
 
-    for i in range(m+1):
-        for j in range(n+1):
+    for i in range(m + 1):
+        for j in range(n + 1):
             if i == 0 or j == 0:
                 c[i][j] = 0
-            elif x[i-1] == y[j-1]:
-                c[i][j] = c[i-1][j-1] + 1
+            elif x[i - 1] == y[j - 1]:
+                c[i][j] = c[i - 1][j - 1] + 1
             else:
                 c[i][j] = max(c[i - 1][j], c[i][j - 1])
 
     return c
 
 
+def walk_lcs_path(matrix, x, y, i, j, curr=''):
+    if i == 0 or j == 0:
+        return curr
+
+    if x[i-1] == y[j-1]:
+        new_curr = walk_lcs_path(matrix, x, y, i - 1, j - 1, curr)
+        return new_curr + x[i - 1]
+    elif matrix[i - 1][j] >= matrix[i][j - 1]:
+        return walk_lcs_path(matrix, x, y, i - 1, j, curr)
+    else:
+        return walk_lcs_path(matrix, x, y, i, j - 1, curr)
+
+
+# https://stackoverflow.com/questions/13214809/pretty-print-2d-list/50257693#50257693
 def pretty_print_matrix(matrix):
     print('\n'.join(['   '.join([str(cell) for cell in row]) for row in matrix]))
 
@@ -47,6 +61,10 @@ if __name__ == "__main__":
     make_report = args['report'] or args['report'] == "true"
     report_file = args["report_file"]
     run_tests = args['test'] or args['test'] == "true"
-    generate_lcs_table("ABCBDAB", "BDCABA")
-
-
+    x = "ABCBDAB"
+    y = "BDCABA"
+    matrix = generate_lcs_table(x, y)
+    pretty_print_matrix(matrix)
+    i = len(matrix) - 1
+    j = len(matrix[0]) - 1
+    print(walk_lcs_path(matrix, x, y, i, j))
